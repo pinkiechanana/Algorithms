389d8827bf01513e48ef8102bc695bc78d07795b
ChananaBanana <hwane94@gmail.com>
2022-11-30 20:06:12 +0900

    added baekjoon

---
+++ src/baek_joon_qa/Q2439.java
@@ -0,0 +1,24 @@
+package baek_joon_qa;// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+// 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+// unfinished..!
+
+import java.util.Scanner;
+
+public class Q2439 {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        scan.close();
+
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
