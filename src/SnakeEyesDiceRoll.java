87e789ded29e5385bbe9193fa34916c1568b3423
ChananaBanana <hwane94@gmail.com>
2022-12-01 00:48:41 +0900

    dice roll algorithm
    snake eye dice roll added

---
+++ src/SnakeEyesDiceRoll.java
@@ -0,0 +1,23 @@
+public class SnakeEyesDiceRoll {
+    public static void main(String[] args) {
+
+        int roll1;
+        int roll2;
+        int count = 0;
+
+        while(true) {
+            roll1 = (int) (6*Math.random()+1);
+            System.out.println("First roll: " + roll1);
+            roll2 = (int) (6*Math.random()+1);
+            System.out.println("Second roll: " + roll2);
+            count++;
+            System.out.println("Count: " + count);
+            int sum = roll1 + roll2;
+            System.out.println("Sum: " + sum);
+            if (sum==2) {
+                break;
+            }
+        }
+        System.out.println("It took " + count + " rolls for a snake-eye.");
+    }
+}
--- src/baek_joon_qa/Testing.java
+++
@@ -1,7 +0,0 @@
-package baek_joon_qa;
-
-public class Testing {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
